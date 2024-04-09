ba95fae06e2bd53701f60c5b101791e05d17a09f
DiscreteTom <discrete_tom@outlook.com>
2024-04-09 19:32:52 +0800

    chore: use bash instead of sh

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() {
             if req.uri().path() == "/2018-06-01/runtime/invocation/next" {
                 let cmd = std::env::var("AWS_LAMBDA_POST_RUNNER_COMMAND")
                     .expect("No command found for aws-lambda-post-runner");
-                Command::new("/bin/sh")
+                Command::new("/bin/bash")
                     .arg("-c")
                     .arg(cmd)
                     .output()
