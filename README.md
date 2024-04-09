20174a857eb3a848da7d6ff67f17516988a81ec3
DiscreteTom <discrete_tom@outlook.com>
2024-04-09 19:31:02 +0800

    feat: finish MVP

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# aws-lambda-post-runner
+# AWS Lambda Post Runner
 Run a command after the lambda handler function returns.
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use aws_lambda_runtime_proxy::{LambdaRuntimeApiClient, Proxy};
+use tokio::process::Command;
+
+#[tokio::main]
+async fn main() {
+    Proxy::default()
+        .spawn()
+        .await
+        .server
+        .serve(|req| async {
+            if req.uri().path() == "/2018-06-01/runtime/invocation/next" {
+                let cmd = std::env::var("AWS_LAMBDA_POST_RUNNER_COMMAND")
+                    .expect("No command found for aws-lambda-post-runner");
+                Command::new("/bin/sh")
+                    .arg("-c")
+                    .arg(cmd)
+                    .output()
+                    .await
+                    .unwrap();
+            }
+
+            LambdaRuntimeApiClient::forward(req).await
+        })
+        .await
+}
