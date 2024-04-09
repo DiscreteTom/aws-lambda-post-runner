8b719760109f3e4af4bbc7fa83e7ce09e8482f93
DiscreteTom <discrete_tom@outlook.com>
2024-04-09 11:56:53 +0000

    fix: fix main logic

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,36 @@ use tokio::process::Command;
 
 #[tokio::main]
 async fn main() {
+    let cmd = std::env::var("AWS_LAMBDA_POST_RUNNER_COMMAND")
+        .expect("No command found for aws-lambda-post-runner");
+
     Proxy::default()
         .spawn()
         .await
         .server
-        .serve(|req| async {
-            if req.uri().path() == "/2018-06-01/runtime/invocation/next" {
-                let cmd = std::env::var("AWS_LAMBDA_POST_RUNNER_COMMAND")
-                    .expect("No command found for aws-lambda-post-runner");
-                Command::new("/bin/bash")
-                    .arg("-c")
-                    .arg(cmd)
-                    .output()
-                    .await
-                    .unwrap();
-            }
+        .serve(move |req| {
+            let cmd = cmd.clone();
 
-            LambdaRuntimeApiClient::forward(req).await
+            async move {
+                let path = req.uri().path();
+                let is_handler_response = path.starts_with("/2018-06-01/runtime/invocation/")
+                    && path.ends_with("/response");
+
+                let res = LambdaRuntimeApiClient::forward(req).await;
+
+                if is_handler_response {
+                    // current request is a handler's response,
+                    // before proceed, run the command
+                    Command::new("/bin/bash")
+                        .arg("-c")
+                        .arg(cmd)
+                        .output()
+                        .await
+                        .unwrap();
+                }
+
+                res
+            }
         })
         .await
 }
